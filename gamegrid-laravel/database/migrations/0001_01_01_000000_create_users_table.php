673be1627f31afb977968c06936f7d3567c41648
ikroupie <168979831+ikroupie@users.noreply.github.com>
2024-10-07 19:07:30 +0300

    user,fields,booking db and api

--- gamegrid-laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ gamegrid-laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('address');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- gamegrid-laravel/routes/web.php
+++ gamegrid-laravel/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Http\Resources\FieldResource;
+use App\Models\Field;
+use App\Http\Resources\Field_bookingResource;
+use App\Models\Field_Booking;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user/{id}', function (string $id) {
+    return new UserResource(User::findOrFail($id));
+});
+
+Route::get('/field/{id}', function (string $id) {
+    return new FieldResource(Field::findOrFail($id));
+});
+
+Route::get('/booking/{id}', function (string $id) {
+    return new Field_bookingResource(Field_Booking::findOrFail($id));
 });
